697e26f6dd6ebf0ba9c4095fd9965993d20abe05
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-18 21:30:09 +0100

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "a94e75c3-eb53-4976-9cbf-eac8d430d6d4"
-  client_id       = "0b639f67-04d2-43d6-97a7-a29446318a9e"
-  client_secret   = "3COqYkTEJWRj11jkvYGojJCNh.RjnVNGIB"
-  tenant_id       = "ed0e3414-6e56-454c-8a82-eb80befb738b"
-}
