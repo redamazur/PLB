4becd35723d00330081eabf83ebeeffe623c10fc
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-18 20:19:20 +0100

    Update provider.tf

--- Dev_Vm/provider.tf
+++ Dev_Vm/provider.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
   features {}
-  subscription_id = "a94e75c3-eb53-4976-9cbf-eac8d430d6d4"
-  client_id       = "0b639f67-04d2-43d6-97a7-a29446318a9e"
-  client_secret   = "3COqYkTEJWRj11jkvYGojJCNh.RjnVNGIB"
+  subscription_id = "745e6c7c-e3ac-4a0d-9008-209fea6dfaeb"
+  client_id       = "b1e3d985-d077-4666-aa08-687d1a26b68b"
+  client_secret   = "W2iFVEBPh2OJpARL--p8c1FQZjFNG2p~ii"
   tenant_id       = "ed0e3414-6e56-454c-8a82-eb80befb738b"
+
 }
