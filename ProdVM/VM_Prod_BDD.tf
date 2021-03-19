69d5379e33aba7c06f3b4bf210d50ae58de9e438
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-19 10:41:24 +0100

    Update VM_Prod_BDD.tf

--- ProdVM/VM_Prod_BDD.tf
+++ ProdVM/VM_Prod_BDD.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "Prod-Bdd-vm" {
     location              = data.azurerm_resource_group.rg_Dev.location
     resource_group_name   = data.azurerm_resource_group.rg_Dev.name
     network_interface_ids = [azurerm_network_interface.Prod_Bdd_01_nic.id]
-    size                  = "Standard_B1ms"
+    size                  = "Standard_DS1_v2"
 
     os_disk {
         name              = "Prod-Bdd-Disk"
